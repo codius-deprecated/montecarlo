cd5b5df1474408c6cb215a2a013b2b6c076528bd
Torrie Fischer <torrie@ripple.com>
2015-02-03 17:25:40 -0800

    add a queue processor to web thread

--- index.js
+++ index.js
@@ -86,3 +86,5 @@ app.get('/crawl', function(req, res) {
 app.listen(app.get('port'), function() {
   console.log('Dashboard is running at localhost:' + app.get('port'));
 });
+
+queue.processNextPullRequest().then(queue.processNextPullRequest.bind(queue));
