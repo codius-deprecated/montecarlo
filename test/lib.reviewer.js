536ed323ccfbeb16f959903164e5067effd85293
Torrie Fischer <torrie@ripple.com>
2015-03-12 17:22:28 -0700

    Test setting pr metadata

--- test/lib.reviewer.js
+++ test/lib.reviewer.js
@@ -6,6 +6,7 @@ var replay = require('replay');
 var expect = require('chai').expect;
 var lgtm = require('../lib/reviewers/lgtm');
 var github = require('../lib/github');
+var redis = require('../lib/redis');
 var PullRequestReviewer = require('../lib/reviewer').PullRequestReviewer;
 
 var MockProcessor = function() {
@@ -19,10 +20,16 @@ MockProcessor.prototype = {
 };
 
 describe('PullRequestReviewer', function() {
+  var r;
+
+  beforeEach(function() {
+    r = new PullRequestReviewer(github, 'codius', 'codius-host', redis);
+    return redis.delAsync('pull-requests');
+  });
+
   describe('#reviewAll', function() {
     it('reviews a list of pull requests', function() {
       var proc = new MockProcessor();
-      var r = new PullRequestReviewer(github, 'codius', 'codius-host');
       r.addProcessor(proc);
       return expect(r.reviewAll('all')).to.be.fulfilled.then(function() {
         expect(proc.seen_ids).to.deep.equal([
@@ -33,6 +40,51 @@ describe('PullRequestReviewer', function() {
           27889886, 27796241, 27546824, 27396449, 27197892, 27130859, 27068571,
           27067652, 26246091, 19262745, 19257943
         ]);
+      });
+    });
+  });
+
+  describe('#setMeta', function() {
+    it('adds an item to the request list', function() {
+      return r.setMeta({id: 1}, 'foo', 'bar').then(function() {
+        return redis.smembersAsync('pull-requests').map(Number);
+      }).then(function(ids) {
+        expect(ids).to.deep.equal([1]);
+      });
+    });
+
+    it('adds several items to the request list', function() {
+      var p = [];
+      for (i = 0; i < 100; i++) { 
+        p.push(r.setMeta({id: i}, 'foo', i));
+      };
+      return bluebird.all(p).then(function() {
+        return redis.smembersAsync('pull-requests').map(Number);
+      }).then(function(ids) {
+        expect(ids).to.have.length(100);
+        expect(ids).to.contain(42);
+      });
+    });
+
+    it('sets a metadata value', function() {
+      return r.setMeta({id: 1}, 'foo', 'bar').then(function() {
+        return redis.smembersAsync('pull-requests').map(Number);
+      }).then(function(ids) {
+        return redis.hgetallAsync('pr:'+ids[0])
+      }).then(function(meta) {
+        expect(meta).to.have.property('foo');
+        expect(meta.foo).to.equal('bar');
+      });
+    });
+
+    it('sets a metadata value from a hash', function() {
+      return r.setMeta({id: 1}, {foo: 'bar'}).then(function() {
+        return redis.smembersAsync('pull-requests').map(Number);
+      }).then(function(ids) {
+        return redis.hgetallAsync('pr:'+ids[0])
+      }).then(function(meta) {
+        expect(meta).to.have.property('foo');
+        expect(meta.foo).to.equal('bar');
       });
     });
   });
