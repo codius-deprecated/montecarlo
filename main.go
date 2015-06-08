40da09fe3bc0703131f871d7e57dd5f21f7ddf18
Torrie Fischer <torrie@ripple.com>
2015-06-08 11:00:12 -0700

    Implement hook enabling

--- main.go
+++ main.go
@@ -1,83 +1,107 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "strings"
-    "github.com/google/go-github/github"
-    "github.com/golang/oauth2"
-    "./repolist"
+	"./repolist"
+	"fmt"
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+	"os"
+	"strings"
 )
 
 type tokenSource struct {
-  token *oauth2.Token
+	token *oauth2.Token
 }
 
 func (t *tokenSource) Token() (*oauth2.Token, error) {
-  return t.token, nil
+	return t.token, nil
 }
 
 type command struct {
-  Owner *string
-  Args []string
+	Owner *string
+	Args  []string
 }
 
-func extractCommands(s *string) ([]string) {
-  ret := make([]string, 0)
+func extractLGTMs(s *string) int {
+	sum := 0
+	for _, line := range strings.Split(*s, "\n") {
+		if strings.Contains(line, "LGTM") || strings.Contains(line, ":+1:") {
+			sum++
+		}
+	}
+	return sum
+}
 
-  for _, line := range strings.Split(*s, "\n") {
-    if strings.HasPrefix(line, "+r") {
-      ret = append(ret, line)
-    } else if strings.Contains(line, "LGTM") || strings.Contains(line, ":+1:") {
-      ret = append(ret, line)
-    }
-  }
-  return ret
+func extractCommands(s *string) []string {
+	ret := make([]string, 0)
+
+	for _, line := range strings.Split(*s, "\n") {
+		if strings.HasPrefix(line, "+r") || strings.HasPrefix(line, "-r") {
+			ret = append(ret, line)
+		}
+	}
+	return ret
 }
 
 func main() {
-  token := os.Getenv("GITHUB_TOKEN")
+	token := os.Getenv("GITHUB_TOKEN")
 
-  if len(token) == 0 {
-    panic("No github token")
-  }
-  ts := &tokenSource {
-    &oauth2.Token {AccessToken: token},
-  }
+	if len(token) == 0 {
+		panic("No github token")
+	}
+	ts := &tokenSource{
+		&oauth2.Token{AccessToken: token},
+	}
 
-  tc := oauth2.NewClient(oauth2.NoContext, ts)
-  client := github.NewClient(tc)
+	tc := oauth2.NewClient(oauth2.NoContext, ts)
+	client := github.NewClient(tc)
 
-  repos := repolist.New(client)
+	repos := repolist.New(client)
 
-  for _, repo := range *repos.List() {
-    fmt.Println("Repo", *repo.FullName)
-    prs, _, _ := client.PullRequests.List(*repo.Owner.Login, *repo.Name, nil)
+	for _, repo := range *repos.List() {
+		fmt.Println("Repo", *repo.FullName)
+		prs, _, _ := client.PullRequests.List(*repo.Owner.Login, *repo.Name, nil)
 
-    for _, pr := range prs {
-      commands := make([]command, 0)
+		for _, pr := range prs {
+			commands := make([]command, 0)
 
-      fmt.Println("\t", *pr.State, pr.Mergeable, *pr.Number, *pr.Title)
-      comments, _, _ := client.PullRequests.ListComments(*repo.Owner.Login,
-          *repo.Name, *pr.Number, nil)
+			fmt.Println("\t", *pr.State, pr.Mergeable, *pr.Number, *pr.Title)
+			comments, _, _ := client.Issues.ListComments(*repo.Owner.Login,
+				*repo.Name, *pr.Number, nil)
 
-      for _, cmds := range extractCommands(pr.Body) {
-        commands = append(commands, command{pr.User.Login, strings.Split(cmds, " ")})
-      }
+			lgtmCount := 0
 
-      for _, comment := range comments {
-        for _, cmds := range extractCommands(comment.Body) {
-          commands = append(commands, command{comment.User.Login, strings.Split(cmds, " ")})
-        }
-      }
+			for _, cmds := range extractCommands(pr.Body) {
+				commands = append(commands, command{pr.User.Login, strings.Split(cmds, " ")})
+			}
 
-      for _, cmd := range commands {
-        if cmd.Args[0] == "+r" && len(cmd.Args) == 1 {
-          fmt.Println("\t\tReview requested!")
-        } else {
-          fmt.Println("\t\tUNKNOWN COMMAND:", cmd.Args)
-        }
-      }
-    }
-  }
+			for _, comment := range comments {
+				lgtmCount += extractLGTMs(comment.Body)
+				for _, cmds := range extractCommands(comment.Body) {
+					commands = append(commands, command{comment.User.Login, strings.Split(cmds, " ")})
+				}
+			}
+
+			reviewRequested := false
+
+			for _, cmd := range commands {
+				if cmd.Args[0] == "+r" && len(cmd.Args) == 1 {
+					reviewRequested = true
+				} else if cmd.Args[0] == "-r" && len(cmd.Args) == 1 {
+					reviewRequested = false
+				} else {
+					fmt.Println("\t\tUNKNOWN COMMAND:", cmd.Args)
+				}
+			}
+			if reviewRequested {
+				fmt.Println("\t\tReview requested!")
+				fmt.Println("\t\tLGTMs:", lgtmCount)
+				if lgtmCount >= 1 {
+					fmt.Println("\t\t\tREADY FOR MERGE!!!")
+				}
+			} else {
+				fmt.Println("\t\tReview requested!")
+			}
+		}
+	}
 }
--- repolist/repolist.go
+++ repolist/repolist.go
@@ -1,29 +1,65 @@
 package repolist
 
 import (
-  "github.com/google/go-github/github"
+	"fmt"
+	"github.com/google/go-github/github"
 )
 
 type RepositoryList struct {
-  client *github.Client
+	client *github.Client
 }
 
 func New(client *github.Client) *RepositoryList {
-  ret := new(RepositoryList)
-  ret.client = client
-  return ret
+	ret := new(RepositoryList)
+	ret.client = client
+	return ret
 }
 
 func (self *RepositoryList) List() *[]github.Repository {
-  var repolist = make([]github.Repository, 0)
+	var repolist = make([]github.Repository, 0)
 
-  teams , _, _ := self.client.Organizations.ListUserTeams(nil)
-  for _, team := range teams {
-    repos, _, _ := self.client.Organizations.ListTeamRepos(*team.ID, nil)
-    for _, repo := range repos {
-      repolist = append(repolist, repo)
-    }
-  }
+	teams, _, _ := self.client.Organizations.ListUserTeams(nil)
+	for _, team := range teams {
+		repos, _, _ := self.client.Organizations.ListTeamRepos(*team.ID, nil)
+		for _, repo := range repos {
+			repolist = append(repolist, repo)
+		}
+	}
 
-  return &repolist
+	return &repolist
+}
+
+func (self *RepositoryList) EnableHooks() {
+	for _, repo := range *self.List() {
+		self.EnableHook(&repo)
+	}
+}
+
+func (self *RepositoryList) EnableHook(repo *github.Repository) {
+	hooks, _, _ := self.client.Repositories.ListHooks(*repo.Owner.Login, *repo.Name, nil)
+
+	fmt.Println("Checking hooks on", *repo.FullName)
+	hasHook := false
+
+	for _, hook := range hooks {
+		if *hook.Name == "web" {
+			fmt.Println("\tHook:", hook.Config["url"])
+			if hook.Config["url"] == "http://build.codius.org/github-hook" {
+				hasHook = true
+			}
+		}
+	}
+
+	if !hasHook {
+		fmt.Println("\tNeeds a hook!")
+		name := "web"
+		hook := github.Hook{
+			Name: &name,
+			Config: map[string]interface{}{
+				"url": "http://build.codius.org/github-hook",
+			},
+			Events: []string{"*"},
+		}
+		self.client.Repositories.CreateHook(*repo.Owner.Login, *repo.Name, &hook)
+	}
 }
